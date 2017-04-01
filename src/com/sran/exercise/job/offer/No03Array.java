5d750f7b6f692215652f3c37c6232dfdc1b905a1
Sran Liu <sranliu1993@gmail.com>
2017-04-01 17:38:06 +0900

    optimize the no3 question

--- src/com/sran/exercise/job/offer/No03Array.java
+++ src/com/sran/exercise/job/offer/No03Array.java
@@ -5,45 +5,36 @@ public class No03Array {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int [][] array = {{1,2,8,9,10},{2,4,9,12,13},{4,7,10,13,14},{6,8,11,15,16}};
-		find(array, 20);
+		find(array, 14);
 	}
 
 	@SuppressWarnings("unused")
 	private static boolean find(int[][] array, int num) {
-		int i=0;
-		int j;
-		
-			for (j=array[0].length-1; j>=0; j--) {
-				
-				if (array[i][j]>num) {
-					continue;
-				} else if (array[i][j]<num) {
-					break;
-				} else if (array[i][j] == num){
-					System.out.println(i + "," + j);
-					return true;
-				} else {
-					System.out.println("no");
-					return false;
-				}
-			}
-			for(i=0; i<array.length; i++) {
-				if (array[i][j]<num)
-					continue;
-				else if (array[i][j] > num) {
-					System.out.println("no");
-					return false;
-				}
-				else {
-					System.out.println(i + "," + j);
-					return true;
-				}
-			}
-			System.out.println("no");
-			return false;
+		int i = 0;
+		int j = array[0].length-1;
+		/*
+		 * 如果该数小于被扫描的数
+		 * 则代表该数一定在左侧
+		 * 如果该数大于被扫描的数
+		 * 则代表该数一定在下侧
+		 */
+		while(i<array.length && j>=0) {
 			
+			if (num<array[i][j]) {
+				j--;
+				continue;
+			} else if (num>array[i][j]) {
+				i++;
+				continue;
+			} else if (num==array[i][j]) {
+				System.out.println("coordinate is " + "(" + (i+1) + "," + (j+1) + ")");
+				return true;
+			}
+			
+		}
 		
-		
+		System.out.println("not fonud!");
+		return false;
 		
 	}
 
